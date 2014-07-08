d49ee396a6e6f406d7523789daec701d82e3030d
KevinSo <kruny1001@gmail.com>
2014-07-08 14:45:51 -0500

    7_8 2

--- public/modules/galleries/config/galleries.client.routes.js
+++ public/modules/galleries/config/galleries.client.routes.js
@@ -2,28 +2,28 @@
 
 //Setting up route
 angular.module('galleries').config(['$stateProvider',
-	function($stateProvider) {
-		// Galleries state routing
-		$stateProvider.
-		state('listGalleries', {
-			url: '/galleries',
-			templateUrl: 'modules/galleries/views/list-galleries.client.view.html'
-		}).
-        state('testGallery', {
-            url: '/galleries/test',
-            templateUrl: 'modules/galleries/views/test-gallery.client.view.html'
-        }).
-		state('createGallery', {
-			url: '/galleries/create',
-			templateUrl: 'modules/galleries/views/create-gallery.client.view.html'
-		}).
-		state('viewGallery', {
-			url: '/galleries/:galleryId',
-			templateUrl: 'modules/galleries/views/view-gallery.client.view.html'
-		}).
-		state('editGallery', {
-			url: '/galleries/:galleryId/edit',
-			templateUrl: 'modules/galleries/views/edit-gallery.client.view.html'
-		});
-	}
+    function($stateProvider) {
+        // Galleries state routing
+        $stateProvider.
+            state('listGalleries', {
+                url: '/galleries',
+                templateUrl: 'modules/galleries/views/list-galleries.client.view.html'
+            }).
+            state('testGallery', {
+                url: '/galleries/test',
+                templateUrl: 'modules/galleries/views/test-gallery.client.view.html'
+            }).
+            state('createGallery', {
+                url: '/galleries/create',
+                templateUrl: 'modules/galleries/views/create-gallery.client.view.html'
+            }).
+            state('viewGallery', {
+                url: '/galleries/:galleryId',
+                templateUrl: 'modules/galleries/views/view-gallery.client.view.html'
+            }).
+            state('editGallery', {
+                url: '/galleries/:galleryId/edit',
+                templateUrl: 'modules/galleries/views/edit-gallery.client.view.html'
+            });
+    }
 ]);
