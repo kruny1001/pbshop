104976a0a90475b0316e4efa624049f4ef57f1d2
kruny1001 <kruny1001@gmail.com>
2014-08-08 14:03:58 -0500

    (deployment) Rotation 180 degree

--- public/dist/application.js
+++ public/dist/application.js
@@ -837,6 +837,19 @@ angular.module('galleries').directive('hideMe', [
       });
     };
   }
+]).directive('expandMe', [
+  '$animate',
+  function ($animate) {
+    return function (scope, element, attrs) {
+      scope.$watch(attrs.expandMe, function (newVal) {
+        if (newVal) {
+          $animate.addClass(element, 'expand');
+        } else {
+          $animate.removeClass(element, 'expand');
+        }
+      });
+    };
+  }
 ]).directive('menulist', [
   '$animate',
   function ($animate) {
@@ -844,8 +857,7 @@ angular.module('galleries').directive('hideMe', [
       restrict: 'E',
       scope: {
         'source': '=',
-        'picture': '=',
-        'hideMe': '&'
+        'picture': '='
       },
       templateUrl: '/modules/galleries/directives/menulist/menulist.html',
       link: function (scope, element, attr, ctrl) {
@@ -861,11 +873,18 @@ angular.module('galleries').directive('hideMe', [
         '$scope',
         '$element',
         function ($scope, $element) {
+          $scope.isExpanded = false;
+          $scope.expandIt = function () {
+            $scope.isExpanded = !$scope.isExpanded;
+          };
+          console.log($element);
           $element.bind('click', function (elem) {
             if ($scope.picture === true) {
               $scope.picture = false;
+              TweenMax.to(elem, 2, { opacity: 0.1 });
             } else {
               $scope.picture = true;
+              TweenMax.to(elem, 2, { opacity: 1 });
             }
             $scope.$apply();
           });
@@ -876,10 +895,28 @@ angular.module('galleries').directive('hideMe', [
 ]).animation('.fade', function () {
   return {
     addClass: function (element, className) {
+      TweenMax.to(element, 2, {
+        rotationY: '+=180',
+        ease: Linear.easeNone
+      });
       TweenMax.to(element, 1, { opacity: 0 });
     },
     removeClass: function (element, className) {
+      TweenMax.to(element, 2, {
+        rotationY: '-=180',
+        ease: Linear.easeNone
+      });
       TweenMax.to(element, 1, { opacity: 1 });
+    }
+  };
+}).animation('.expand', function () {
+  return {
+    addClass: function (element, className) {
+      TweenMax.to(element, 1, { left: 0 });
+      TweenMax.to(element, 1, { opacity: 1 });
+    },
+    removeClass: function (element, className) {
+      TweenMax.to(element, 1, { left: 20 });
     }
   };
 });/*
