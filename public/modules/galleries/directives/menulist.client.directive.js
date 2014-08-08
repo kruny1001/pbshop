f1828ac6589ec79512db8772eb19fd56c19acfe9
kruny1001 <kruny1001@gmail.com>
2014-08-08 13:59:03 -0500

    Rotation 180 degree

--- public/modules/galleries/directives/menulist.client.directive.js
+++ public/modules/galleries/directives/menulist.client.directive.js
@@ -16,17 +16,28 @@ angular.module('galleries')
             })
         }
     })
+    .directive("expandMe", function($animate){
+        return function(scope, element, attrs) {
+            scope.$watch(attrs.expandMe, function(newVal) {
+                if (newVal) {
+                    $animate.addClass(element, "expand")
+                } else {
+                    $animate.removeClass(element, "expand")
+                }
+            })
+        }
+    })
     .directive('menulist', ['$animate',
         function($animate) {
             return {
                 restrict: 'E',
                 scope: {
                     'source': '=',
-                    'picture':'=',
-                    'hideMe':'&'
+                    'picture':'='
                 },
                 templateUrl: '/modules/galleries/directives/menulist/menulist.html',
                 link: function(scope, element, attr, ctrl) {
+
                     scope.$watch(attr.hideMe, function(newVal) {
                         if (newVal) {
                             $animate.addClass(element, "fade")
@@ -36,12 +47,21 @@ angular.module('galleries')
                     })
                 },
                 controller: function($scope, $element){
+                    $scope.isExpanded = false;
+                    $scope.expandIt = function() {
+                        $scope.isExpanded = !$scope.isExpanded;
+                    }
+
+                    console.log($element);
+
                     $element.bind('click', function(elem){
                         if($scope.picture===true) {
                             $scope.picture = false;
+                            TweenMax.to(elem, 2, {opacity:0.1});
                         }
                         else {
                             $scope.picture = true;
+                            TweenMax.to(elem, 2, {opacity:1});
                         }
                         $scope.$apply();
                     });
@@ -52,10 +72,25 @@ angular.module('galleries')
     .animation('.fade', function() {
         return{
             addClass: function(element, className) {
+                TweenMax.to(element, 2, {rotationY:"+=180", ease:Linear.easeNone});
                 TweenMax.to(element, 1, {opacity: 0});
             },
             removeClass: function(element, className) {
+                TweenMax.to(element, 2, {rotationY:"-=180", ease:Linear.easeNone});
                 TweenMax.to(element, 1, {opacity: 1});
+            }
+
+        }
+    })
+    .animation('.expand', function() {
+        return{
+            addClass: function(element, className) {
+                TweenMax.to(element, 1, {left:0});
+                TweenMax.to(element, 1, {opacity: 1});
+            },
+            removeClass: function(element, className) {
+                TweenMax.to(element, 1, {left:20});
+
             }
 
         }
