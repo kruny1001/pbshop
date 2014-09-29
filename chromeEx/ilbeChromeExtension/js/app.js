9c09ab094a26ab9cd53d0476a06ca6443376ed3d
kruny1001 <kruny1001@gmail.com>
2014-09-29 17:07:40 -0500

    9/26 chrome app update

--- chromeEx/ilbeChromeExtension/js/app.js
+++ chromeEx/ilbeChromeExtension/js/app.js
@@ -5,23 +5,25 @@
 /**
  * Created by KevinSo on 9/26/2014.
  */
-
+'use strict';
+var body = '';
 var IlbeApp = angular.module('ilbeApp', []);
 
 IlbeApp.config( [
-    '$compileProvider',
-    function( $compileProvider )
+    '$compileProvider', '$httpProvider',
+    function( $compileProvider, $httpProvider )
     {
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
         // Angular -1.2.0-rc2 : /^\s*(https?|ftp|file):|data:image\//
-        var currentImgSrcSanitizationWhitelist = $compileProvider.imgSrcSanitizationWhitelist();
-        newImgSrcSanitizationWhiteList = currentImgSrcSanitizationWhitelist.toString().slice(0,-1)+'|filesystem:chrome-extension:'+'|blob:chrome-extension%3A'+currentImgSrcSanitizationWhitelist.toString().slice(-1);
+        //var currentImgSrcSanitizationWhitelist = $compileProvider.imgSrcSanitizationWhitelist();
+        //newImgSrcSanitizationWhiteList = currentImgSrcSanitizationWhitelist.toString().slice(0,-1)+'|filesystem:chrome-extension:'+'|blob:chrome-extension%3A'+currentImgSrcSanitizationWhitelist.toString().slice(-1);
         //console.log("Changing imgSrcSanitizationWhiteList from "+currentImgSrcSanitizationWhitelist+" to "+newImgSrcSanitizationWhiteList);
-
-        $compileProvider.imgSrcSanitizationWhitelist(newImgSrcSanitizationWhiteList);
+        //$compileProvider.imgSrcSanitizationWhitelist(newImgSrcSanitizationWhiteList);
     }
 ]);
 
-IlbeApp.factory('myXHRService', function($http){
+/*IlbeApp.factory('myXHRService', function($http){
     delete $http.defaults.headers.common['X-requested-With'];
     return {
         getData: function(url){
@@ -33,17 +35,19 @@ IlbeApp.factory('myXHRService', function($http){
         }
     }
 });
+*/
 
 IlbeApp.provider('Weather', function(){
     this.$get = function($q, $http) {
         var self = this;
         return {
-            getWeatherForecast: function() {
+            getWeatherForecast: function(url) {
                 var d = $q.defer();
                 $http({
                     method: 'GET',
-                    url: 'http://www.ilbe.com/jjal/',
-                    cache: true
+                    url: 'http://www.ilbe.com/'+url+'',
+                    cache: true,
+                    responseType: 'document'
                 }).success(function(data) {
                     d.resolve(data);
                 }).error(function(err) {
@@ -53,17 +57,37 @@ IlbeApp.provider('Weather', function(){
             }
         }
     }
-})
-
-IlbeApp.config(function($httpProvider){
-    $httpProvider.defaults.useXDomain = true;
-    $httpProvider.defaults.headers.common["X-Requested-With"] = 'XMLHttpRequest';
 });
 
-IlbeApp.controller('MainCtrl', function($scope, $timeout, $http, Weather, myXHRService) {
+IlbeApp.controller('MainCtrl', function($scope, $timeout, $http, Weather) {
     // Build the date object
-    $scope.date = {};
+    document.querySelector('webview').
+        setUserAgentOverride('Mozilla/5.0 (Linux; U; Android 4.0.3; ko-kr; LG-L160L Build/IML74K) AppleWebkit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30');
 
+    $scope.date = {};
+    $scope.boards= [
+      {name:"일베", url:"ilbe"},
+      {name:"짤방", url:"jjal"},
+      {name:"컴게", url:"4393424350"}
+    ];
+    $scope.myBoard = $scope.boards[0];
+    
+    $scope.changeValue =function(newValue) {
+      console.log(newValue);
+      Weather.getWeatherForecast(newValue.url).then(function(data){
+        console.log(data);
+        $scope.articles = $(data).find('#bbs_ilbe_0').find('li a');
+        $scope.articleBody = $(data).find('.contentBody div');
+        body = $scope.articleBody[0];
+        console.log($scope.articles);
+        console.log($scope.articleBody);
+        $scope.contentBody = $scope.articleBody[0];
+        //webview.src = $scope.contentBody;
+    });
+    
+      
+    }
+    
     /*
     $http({method: 'GET', url: 'www.google.com', headers: {'Authorization': 'Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ=='}}).success(function(data){
         $scope.article=data;
@@ -85,12 +109,22 @@ IlbeApp.controller('MainCtrl', function($scope, $timeout, $http, Weather, myXHRS
     // Kick off the update function
     updateTime();
 
-    Weather.getWeatherForecast().then(function(data){
+    Weather.getWeatherForecast($scope.myBoard.url).then(function(data){
         console.log(data);
         $scope.articles = $(data).find('#bbs_ilbe_0').find('li a');
         console.log($scope.articles);
+        var webview=document.querySelector("webview");
+        webview.addEventListener('mouseover', function() {
+          console.log('over');
+          webview.executeScript({ code: "document.body.style.visibility = 'visible'" });
+          webview.executeScript({ code: "document.body.style.backgroundColor  = 'white'" });
+        });
+        webview.addEventListener('mouseout', function() {
+          console.log('out');
+          webview.executeScript({ code: "document.body.style.visibility = 'hidden'" });
+          webview.executeScript({ code: "document.body.style.backgroundColor  = '#444" });
+        });
 
     });
-
-
 });
+
--- chromeEx/ilbeChromeExtension/main.js
+++ chromeEx/ilbeChromeExtension/main.js
@@ -1,11 +1,39 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('weather.html', {
+
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+window.chrome.app.runtime.onLaunched.addListener(function() {
+  runApp();
+});
+
+/**
+ * Listens for the app restarting then re-creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ */
+window.chrome.app.runtime.onRestarted.addListener(function() {
+  runApp();
+});
+
+/**
+ * Creates the window for the application.
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+function runApp() {
+  window.chrome.app.window.create('weather.html', {
     id: 'weather',
     innerBounds: {
-      height: 450,
-      width: 300,
+      height: 750,
+      width: 355,
     },
-    frame:'none',
-    resizable: false
-  });
-});
+    'state': 'maximized'
+    //resizable: false
+  },function(win) {
+       myWin = win;
+       myWin.contentWindow.postMessage('Just wanted to say hey.', '*');
+     });
+}
