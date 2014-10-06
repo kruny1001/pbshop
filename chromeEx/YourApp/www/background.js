1d70fae2a7c7b75af10746f2f1eeacd8b08dae0b
Kevin Son <kruny1001@gmail.com>
2014-10-06 02:15:19 -0500

    CCA deploy on IOS and Android

--- chromeEx/YourApp/www/background.js
+++ chromeEx/YourApp/www/background.js
@@ -1,12 +1,45 @@
 /**
- * Listens for the app launching then creates the window.
- * Ignores the provided window size.
+ * Listens for the app launching then creates the window
  *
- * @see http://developer.chrome.com/trunk/apps/app.window.html
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
  */
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    width: 244,
-    height: 380,
-  });
+window.chrome.app.runtime.onLaunched.addListener(function() {
+    runApp();
 });
+
+/**
+ * Listens for the app restarting then re-creates the window.
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ */
+window.chrome.app.runtime.onRestarted.addListener(function() {
+    runApp();
+});
+
+chrome.runtime.onInstalled.addListener(function() {
+    console.log('installed');
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+    // Do some simple clean-up tasks.
+});
+
+/**
+ * Creates the window for the application.
+ *
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+function runApp() {
+    window.chrome.app.window.create('index.html', {
+        frame: "none",
+        id: 'productEditor',
+        innerBounds: {
+            width: 460,
+            height: 300,
+            left:600,
+            minWidth:220,
+            minHeight:220
+        }
+    });
+}
